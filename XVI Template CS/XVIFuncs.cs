cecda900e1b61decdc383a6839f2a7c145e943ec
DragonXVI <Dragonboyxvi@gmail.com>
2026-07-27 23:43:25 -0400

    test async resource load

--- XVI Template CS/XVIFuncs.cs
+++ XVI Template CS/XVIFuncs.cs
@@ -1,4 +1,6 @@
+using System.Threading.Tasks;
 using Godot;
+using Godot.Collections;
 
 namespace DragonXVI;
 
@@ -20,5 +22,35 @@ public abstract partial class XVIFuncs : GodotObject
         node.SetProcessShortcutInput( enabled );
         node.SetProcessUnhandledInput( enabled );
         node.SetProcessUnhandledKeyInput( enabled );
+    }
+    
+    /// <summary>
+    /// A neat wrapper for the functions in [ResourceLoader].
+    /// A standardized way to load a resource on a thread using await.
+    /// 
+    /// All of the arguments are the same as the resource loader thread ones, including the progress array.
+    /// More of a test than anything, not gdscript friendly qwqqqq
+    /// </summary>
+    public static async Task<Resource> LoadResourceCoroutine( string resourcePath, string typeHint = "", Array progress = [] )
+    {
+        Error err = ResourceLoader.LoadThreadedRequest( resourcePath, typeHint );
+        if ( err != Error.Ok ) {
+            GD.PushError( $"Could not load resource {resourcePath} on thread: {err}" );
+            return null;
+        }
+        
+        while (true) {
+            var loadStatus = ResourceLoader.LoadThreadedGetStatus( resourcePath, progress );
+            if ( loadStatus == ResourceLoader.ThreadLoadStatus.Loaded ) {
+                break;
+            } else if ( loadStatus == ResourceLoader.ThreadLoadStatus.InProgress ) {
+                var tree = (SceneTree)Engine.GetMainLoop();
+                await tree.ToSignal( tree, SceneTree.SignalName.ProcessFrame );
+            } else {
+                return null;
+            }
+        }
+        
+        return ResourceLoader.LoadThreadedGet( resourcePath );
     }
 }
